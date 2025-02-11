72871ad0e95f72195dd8a31ee036d7d8ee3a9604
IG <ifediorachukwu@yahoo.com>
2025-02-11 12:58:19 +0100

    final, less optional

--- employee/employee_manager.py
+++ employee/employee_manager.py
@@ -1,48 +1,57 @@
 from .models import Employee
 from faker import Faker
-import uuid
+
 
 # Create an employee manager class that will manage the employees
-class EmployeeManager():
+class EmployeeManager:
     def __init__(self):
         self.employees = []
 
 # Class Method to Add an employee to the list of employees   
     def add_employee(self, employee):
         for emp in self.employees:
-            if emp.employee_id == employee.employee_id:
+            if emp['id'] == employee['id']:
                 raise ValueError('Employee already exists')
         self.employees.append(employee)
 
 # Class Method to get an employee from the list of employees     
     def get_employee(self, employee_id):
         for employee in self.employees:
-            if employee.employee_id == employee_id:
-                return employee
+            if employee['id'] == employee_id:
+                print (employee)
         return None
     
 # Class Method to get all employees from the list of employees
     def get_all_employees(self):
-        return self.employees
+        print (self.employees)
 
 def main():
     try:
 # An instance of faker to test data
         faker = Faker()
-        employee = Employee (
-            id=faker.random_int(min=1, max=1000),
-            name=input('Enter Employee Name: ').strip(),
-            age=faker.random_int(min=18, max=65),
-            dept=faker.job().strip(),
-            role=faker.random_element(elements=['Manager', 'Developer', 'Designer', 'HR', 'Sales']),
-            salary=faker.random_int(min=1000, max=10000)
-        )
         employee_manager = EmployeeManager()
-        employee_manager.add_employee(employee)
-        print(employee_manager.employees)
+
+        #Add five random employees
+        for x in range(5):
+            employee = Employee (
+                # id=faker.random_int(min=1, max=15),
+                id=x+1,
+                name=faker.name().strip(),  
+                # name=input('Enter Employee Name: ').strip(), # did not use faker so I could test wrong input
+                age=faker.random_int(min=18, max=65),
+                dept=faker.job().strip(),
+                role=faker.random_element(elements=['Manager', 'Developer', 'Designer', 'HR', 'Sales']),
+                salary=faker.random_int(min=1000, max=10000)
+            ).model_dump()
+            employee_manager.add_employee(employee)
+        
+        # Get all employees
+        employee_manager.get_all_employees()
+        # Search for employee with id of 1
+        employee_manager.get_employee(1)
+        
     except Exception as e:
         print(f'Error: {e}')
-
 
 if __name__ == "__main__":
     main()
--- employee/models.py
+++ employee/models.py
@@ -5,7 +5,7 @@ from typing import Literal
 class Employee(BaseModel):
     id: PositiveInt 
     name: str = Field(..., min_length=2)
-    age: int = Field(..., gt=18, lt=65)
+    age: int = Field(..., gt=17, lt=66)
     dept: str
     role: Literal['Manager', 'Developer', 'Designer', 'HR', 'Sales']
     salary: int = Field(..., gt=0)
